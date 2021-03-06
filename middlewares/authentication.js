cac93bdd77ac9c3efea22dee63edd19481a06acc
Andres Ramirez <andres200493@gmail.com>
2021-03-06 13:09:59 -0600

    Mi primer commit

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,6 +3,7 @@ const sequelize = require('../db');
 
 const authenticate = (req, res, next) => {
   const { authorization } = req.headers;
+  console.log(authorization);
   jwt.verify(authorization, process.env.JWT_SECRETKEY, async (err, decoded) => {
     if (err) return res.status(401).json({ message: 'Unauthorized' });
     req.user = await sequelize.models.users.findOne({ where: { id: decoded.userId }});
